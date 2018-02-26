27a062799b2f5be19a80ca250e99f213c21080d1
pjjr201 <pjjr201@exeter.ac.uk>
2018-02-26 16:24:39 +0000

    Changed the \ to /

--- XTrek/src/MenuScreen.java
+++ XTrek/src/MenuScreen.java
@@ -19,33 +19,33 @@ public class MenuScreen extends Screen {
         setBackground(Color.BLACK);
 
         //Creates the whereTo image (Default Selected)
-        whereTo.setIcon(new ImageIcon(this.getClass().getResource("\\images\\whereTo_selected.png")));
+        whereTo.setIcon(new ImageIcon(this.getClass().getResource("images/whereTo_selected.png")));
         whereTo.setBounds(87, 224, 100, 72);
         add(whereTo);
         selectedItem = "whereTo";
 
         //Creates the tripComputer image
-        tripComputer.setIcon(new ImageIcon(this.getClass().getResource("\\images\\tripComputer.png")));
+        tripComputer.setIcon(new ImageIcon(this.getClass().getResource("images/tripComputer.png")));
         tripComputer.setBounds(182, 224, 90, 72);
         add(tripComputer);
 
         //Creates the map image
-        map.setIcon(new ImageIcon(this.getClass().getResource("\\images\\map.png")));
+        map.setIcon(new ImageIcon(this.getClass().getResource("images/map.png")));
         map.setBounds(87, 301, 90, 72);
         add(map);
 
         //Creates the speech image
-        speech.setIcon(new ImageIcon(this.getClass().getResource("\\images\\speech.png")));
+        speech.setIcon(new ImageIcon(this.getClass().getResource("images/speech.png")));
         speech.setBounds(182, 301, 90, 72);
         add(speech);
 
         //Creates the satellite image
-        satellite.setIcon(new ImageIcon(this.getClass().getResource("\\images\\satellite.png")));
+        satellite.setIcon(new ImageIcon(this.getClass().getResource("images/satellite.png")));
         satellite.setBounds(87, 378, 90, 72);
         add(satellite);
 
         //Creates the about image
-        about.setIcon(new ImageIcon(this.getClass().getResource("\\images\\about.png")));
+        about.setIcon(new ImageIcon(this.getClass().getResource("images/about.png")));
         about.setBounds(182, 378, 90, 72);
         add(about);
     }
@@ -60,38 +60,38 @@ public class MenuScreen extends Screen {
         try {
             switch (selectedItem) {
                 case "whereTo": {
-                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\whereTo.png"))));
-                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\tripComputer_selected.png"))));
+                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/whereTo.png"))));
+                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/tripComputer_selected.png"))));
                     selectedItem = "tripComputer";
                     break;
                 }
                 case "tripComputer": {
-                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\tripComputer.png"))));
-                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\map_selected.png"))));
+                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/tripComputer.png"))));
+                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/map_selected.png"))));
                     selectedItem = "map";
                     break;
                 }
                 case "map": {
-                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\map.png"))));
-                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\speech_selected.png"))));
+                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/map.png"))));
+                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/speech_selected.png"))));
                     selectedItem = "speech";
                     break;
                 }
                 case "speech": {
-                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\speech.png"))));
-                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\satellite_selected.png"))));
+                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/speech.png"))));
+                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/satellite_selected.png"))));
                     selectedItem = "satellite";
                     break;
                 }
                 case "satellite": {
-                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\satellite.png"))));
-                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\about_selected.png"))));
+                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/satellite.png"))));
+                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/about_selected.png"))));
                     selectedItem = "about";
                     break;
                 }
                 case "about": {
-                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\about.png"))));
-                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\whereTo_selected.png"))));
+                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/about.png"))));
+                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/whereTo_selected.png"))));
                     selectedItem = "whereTo";
                     break;
                 }
@@ -106,38 +106,38 @@ public class MenuScreen extends Screen {
         try {
             switch (selectedItem) {
                 case "about": {
-                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\about.png"))));
-                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\satellite_selected.png"))));
+                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/about.png"))));
+                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/satellite_selected.png"))));
                     selectedItem = "satellite";
                     break;
                 }
                 case "satellite": {
-                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\satellite.png"))));
-                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\speech_selected.png"))));
+                    satellite.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/satellite.png"))));
+                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/speech_selected.png"))));
                     selectedItem = "speech";
                     break;
                 }
                 case "speech": {
-                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\speech.png"))));
-                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\map_selected.png"))));
+                    speech.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/speech.png"))));
+                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/map_selected.png"))));
                     selectedItem = "map";
                     break;
                 }
                 case "map": {
-                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\map.png"))));
-                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\tripComputer_selected.png"))));
+                    map.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/map.png"))));
+                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/tripComputer_selected.png"))));
                     selectedItem = "tripComputer";
                     break;
                 }
                 case "tripComputer": {
-                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\tripComputer.png"))));
-                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\whereTo_selected.png"))));
+                    tripComputer.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/tripComputer.png"))));
+                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/whereTo_selected.png"))));
                     selectedItem = "whereTo";
                     break;
                 }
                 case "whereTo": {
-                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\whereTo.png"))));
-                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("\\images\\about_selected.png"))));
+                    whereTo.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/whereTo.png"))));
+                    about.setIcon(new ImageIcon(ImageIO.read(this.getClass().getResource("images/about_selected.png"))));
                     selectedItem = "about";
                     break;
                 }
